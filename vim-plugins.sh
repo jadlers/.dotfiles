7d39952a7fd0d7b0eec4dc79c905a26d7889c6c0
Jacob Adlers <jacob.adlers@gmail.com>
2018-01-29 15:04:50 +0100

    (vim) Add clang-format plugin

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -3,6 +3,7 @@
 
 declare -a plugins=(
   "git://github.com/tpope/vim-surround.git" # vim surround
+  "https://github.com/rhysd/vim-clang-format.git" # C family formatter
   "https://github.com/tomtom/tlib_vim.git" # for snippets
   "https://github.com/MarcWeber/vim-addon-mw-utils.git"
   "https://github.com/garbas/vim-snipmate.git"
