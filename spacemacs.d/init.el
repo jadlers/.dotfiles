edc9a53cc0e4a85b58960fec9d4083fcf484166c
Jacob Adlers <jacob.adlers@gmail.com>
2018-08-20 13:06:03 +0200

    (🚀) Set up auto-complete & c++ layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -36,7 +36,9 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layers
    '(
      ;; General
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-tab-key-behavior 'complete
+                      auto-completion-enable-snippets-in-popup t)
      better-defaults
      git
      helm
@@ -63,7 +65,9 @@ This function should only modify configuration layer settings."
      org
 
      ;; Programming
-     c-c++
+     (c-c++ :variables
+            c-c++-enable-clang-support t
+            c-c++-default-mode-for-headers 'c++-mode)
      emacs-lisp
      go
      html
@@ -501,53 +505,79 @@ before packages are loaded."
   (setq standard-indent 2)
   (setq js-indent-level 2)
   )
+
 (defun dotspacemacs/emacs-custom-settings ()
   "Emacs custom settings.
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(git-gutter-fringe+ git-gutter-fringe
- fringe-helper git-gutter+ git-gutter diff-hl browse-at-remote
- yasnippet-snippets xterm-color xkcd ws-butler winum which-key web-mode
- web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package unfill
- typit toc-org tagedit symon sudoku string-inflection stickyfunc-enhance
- srefactor spotify spaceline-all-the-icons smeargle slim-mode shell-pop
- scss-mode sass-mode rjsx-mode reveal-in-osx-finder restart-emacs ranger
- rainbow-delimiters pug-mode prettier-js popwin persp-mode pcre2el
- password-generator paradox pacmacs overseer osx-trash osx-dictionary orgit
- org-ref org-projectile org-present org-pomodoro org-mime org-download
- org-bullets org-brain open-junk-file neotree nameless mwim multi-term move-text
- mmm-mode markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum livid-mode
- link-hint launchctl json-navigator json-mode js2-refactor js-doc indent-guide
- impatient-mode hungry-delete hl-todo highlight-parentheses highlight-numbers
- highlight-indentation helm-xref helm-themes helm-swoop helm-spotify-plus
- helm-rtags helm-purpose helm-projectile helm-mode-manager helm-make
- helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company
- helm-c-yasnippet helm-ag google-translate google-c-style golden-ratio godoctor
- go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc gnuplot
- gitignore-templates gitconfig-mode gitattributes-mode git-timemachine
- git-messenger git-link gh-md fuzzy font-lock+ flyspell-correct-helm
- flycheck-rtags flycheck-pos-tip flx-ido fill-column-indicator fancy-battery
- eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired
- evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-mc
- evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus
- evil-iedit-state evil-goggles evil-exchange evil-escape evil-cleverparens
- evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help
- emmet-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode disaster diminish
- counsel-projectile company-web company-tern company-statistics company-rtags
- company-go company-c-headers company-auctex column-enforce-mode
- clean-aindent-mode clang-format centered-cursor-mode auto-yasnippet
- auto-highlight-symbol auto-dictionary auto-compile aggressive-indent ace-window
- ace-link ace-jump-helm-line ac-ispell 2048-game)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-)
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(package-selected-packages '(git-gutter-fringe+ git-gutter-fringe
+                                 fringe-helper git-gutter+ git-gutter diff-hl
+                                 browse-at-remote yasnippet-snippets xterm-color
+                                 xkcd ws-butler winum which-key web-mode
+                                 web-beautify volatile-highlights
+                                 vi-tilde-fringe uuidgen use-package unfill
+                                 typit toc-org tagedit symon sudoku
+                                 string-inflection stickyfunc-enhance srefactor
+                                 spotify spaceline-all-the-icons smeargle
+                                 slim-mode shell-pop scss-mode sass-mode
+                                 rjsx-mode reveal-in-osx-finder restart-emacs
+                                 ranger rainbow-delimiters pug-mode prettier-js
+                                 popwin persp-mode pcre2el password-generator
+                                 paradox pacmacs overseer osx-trash
+                                 osx-dictionary orgit org-ref org-projectile
+                                 org-present org-pomodoro org-mime org-download
+                                 org-bullets org-brain open-junk-file neotree
+                                 nameless mwim multi-term move-text mmm-mode
+                                 markdown-toc magit-svn magit-gitflow macrostep
+                                 lorem-ipsum livid-mode link-hint launchctl
+                                 json-navigator json-mode js2-refactor js-doc
+                                 indent-guide impatient-mode hungry-delete
+                                 hl-todo highlight-parentheses highlight-numbers
+                                 highlight-indentation helm-xref helm-themes
+                                 helm-swoop helm-spotify-plus helm-rtags
+                                 helm-purpose helm-projectile helm-mode-manager
+                                 helm-make helm-gitignore helm-flx
+                                 helm-descbinds helm-css-scss helm-company
+                                 helm-c-yasnippet helm-ag google-translate
+                                 google-c-style golden-ratio godoctor go-tag
+                                 go-rename go-impl go-guru go-gen-test
+                                 go-fill-struct go-eldoc gnuplot
+                                 gitignore-templates gitconfig-mode
+                                 gitattributes-mode git-timemachine
+                                 git-messenger git-link gh-md fuzzy font-lock+
+                                 flyspell-correct-helm flycheck-rtags
+                                 flycheck-pos-tip flx-ido fill-column-indicator
+                                 fancy-battery eyebrowse expand-region
+                                 evil-visualstar evil-visual-mark-mode
+                                 evil-unimpaired evil-tutor evil-surround
+                                 evil-org evil-numbers evil-nerd-commenter
+                                 evil-mc evil-matchit evil-magit evil-lisp-state
+                                 evil-lion evil-indent-plus evil-iedit-state
+                                 evil-goggles evil-exchange evil-escape
+                                 evil-cleverparens evil-args evil-anzu
+                                 eval-sexp-fu eshell-z eshell-prompt-extras
+                                 esh-help emmet-mode elisp-slime-nav
+                                 editorconfig dumb-jump dotenv-mode disaster
+                                 diminish counsel-projectile company-web
+                                 company-tern company-statistics company-rtags
+                                 company-go company-c-headers company-auctex
+                                 column-enforce-mode clean-aindent-mode
+                                 clang-format centered-cursor-mode
+                                 auto-yasnippet auto-highlight-symbol
+                                 auto-dictionary auto-compile aggressive-indent
+                                 ace-window ace-link ace-jump-helm-line
+                                 ac-ispell 2048-game)))
+
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   )
+  )
