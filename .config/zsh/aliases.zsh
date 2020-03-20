96037badef0113469d7f40093cc21033dc65b750
Jacob Adlers <jacob.adlers@gmail.com>
2020-03-20 18:23:22 +0100

    Add zsh aliases for listing files
    Also a gc = git commit

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,8 +1,13 @@
 alias dfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
+# ls
+alias ll='ls -l'
+alias la='ls -la'
+
 # Git
 alias ga='git add'
 alias gapa='git add --patch'
+alias gc='git commit'
 alias gco='git checkout'
 alias gd='git diff'
 alias gst='git status'
