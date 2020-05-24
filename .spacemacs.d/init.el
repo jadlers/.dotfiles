d94af0c77b5fdf98f3e631a5b4faa6b39a57361a
Jacob Adlers <jacob.adlers@gmail.com>
2020-05-24 13:51:57 +0200

    (🚀) Disable current line globally highlighted

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -567,6 +567,8 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (setq global-hl-line-mode nil)
+
   ;; LSP config
   (setq lsp-inhibit-message t)
 
