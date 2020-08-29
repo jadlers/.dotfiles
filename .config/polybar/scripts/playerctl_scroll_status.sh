b2052acfcb45066f183694fec64b905b5331e1b2
Jacob Adlers <jacob.adlers@gmail.com>
2020-08-29 19:25:29 +0200

    Make spotify into general playerctl scripts

--- .config/polybar/scripts/playerctl_scroll_status.sh
+++ .config/polybar/scripts/playerctl_scroll_status.sh
@@ -3,9 +3,9 @@
 # see man zscroll for documentation of the following parameters
 zscroll -l 60 \
         --delay 0.1 \
-        --match-command "playerctl --player=spotify status" \
+        --match-command "playerctl --player=${1-playerctld} status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
-        --update-check true '/home/jacob/.config/polybar/scripts/spotify_get_status.sh spotify' &
+        --update-check true "/home/jacob/.config/polybar/scripts/playerctl_get_status.sh ${1-playerctld}" &
 
 wait
