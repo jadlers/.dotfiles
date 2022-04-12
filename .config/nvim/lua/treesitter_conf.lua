53bcca1ba95c849286381f2247161e4da125f8af
Jacob Adlers <jacob.adlers@gmail.com>
2022-04-12 21:16:32 +0200

    (nvim) Fix treesitter installed languages
    The "ensure_installed" is no longer available. Using "all" now instead.

--- .config/nvim/lua/treesitter_conf.lua
+++ .config/nvim/lua/treesitter_conf.lua
@@ -1,6 +1,9 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = {
+    enable = true,
+  },
+  indent = {
     enable = true,
   },
 }
