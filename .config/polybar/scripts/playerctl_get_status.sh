a6144f6fb69ffd5ddfdd63763d87eb1798508aa9
Jacob Adlers <jacob.adlers@gmail.com>
2022-04-30 11:06:19 +0200

    (polybar) update media playing information

--- .config/polybar/scripts/playerctl_get_status.sh
+++ .config/polybar/scripts/playerctl_get_status.sh
@@ -4,18 +4,13 @@ FORMAT="{{ title }} - {{ artist }}"
 PLAYER="${1-playerctld}"
 # Merge stdout & stderr
 PLAYER_STATUS="$(playerctl --player="$PLAYER" status 2>&1)"
+info="$(playerctl --player="$PLAYER" metadata --format "$FORMAT")"
 
 case "$PLAYER_STATUS" in
-  Playing|Paused)
-  # Playing)
-    # NOTE: Old version, ratelimited by spotifyd
-    # playerctl --player="$PLAYER" metadata --format "$FORMAT"
-
-    # Works with spotifyd without exhausting requests
-    spt playback --status 
-    ;;
+  Playing)
+    echo "$info" ;;
   Paused)
-    echo "Paused" ;;
+    echo "(Paused) $info" ;;
   *)
     echo "" ;;
 esac
