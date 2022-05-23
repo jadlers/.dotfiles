eb20f179c55eae7db761ca62befb3869d46589a2
Jacob Adlers <jacob.adlers@gmail.com>
2022-05-23 13:41:46 +0200

    (polybar) prepare show mpv playing title

--- .config/polybar/scripts/playerctl_get_status.sh
+++ .config/polybar/scripts/playerctl_get_status.sh
@@ -6,6 +6,10 @@ PLAYER="${1-playerctld}"
 PLAYER_STATUS="$(playerctl --player="$PLAYER" status 2>&1)"
 info="$(playerctl --player="$PLAYER" metadata --format "$FORMAT")"
 
+# Quick hack to make it work for mpv streaming
+# PLAYER_STATUS="$(playerctl status 2>&1)"
+# info="$(playerctl metadata --format "{{ title }}")"
+
 case "$PLAYER_STATUS" in
   Playing)
     echo "$info" ;;
